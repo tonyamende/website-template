5e1f5284f7b13460ea87daced3af9085c4ddf4ef
Tony Amende <47224894+tonyamende@users.noreply.github.com>
2025-07-25 22:40:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/website-template/', // 👈 Must match your repo name
+  base: '/website-template', // 👈 Must match your repo name
 })

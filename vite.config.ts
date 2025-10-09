406acc16485e86ec30cff4e324e7823d34dff43c
Tony Amende <tony.amende@gmail.com>
2025-10-09 22:30:44 +0200

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/website-template/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {

63e97f8be46759294f3489d22836162687673239
Tony Amende <tony.amende@gmail.com>
2025-10-09 22:42:38 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/website-template/', // 🔥 Add this line — replace <repo-name>!
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
